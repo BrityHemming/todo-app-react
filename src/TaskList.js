3c936d9e84eefa867d9ee4a906661f1f9baaa243
James Churchill <james@smashdev.com>
2022-06-15 13:22:16 -0700

    Update the TaskList and Task components

--- src/TaskList.js
+++ src/TaskList.js
@@ -3,7 +3,7 @@ import './TaskList.css';
 import {useState} from 'react';
 import AddTask from "./AddTask";
 
-//rfcs enter is a shortcut for creating a functional component 
+
 // function TaskList(){
 //     return (<section className="task-container">
 //         <Task/>
@@ -16,14 +16,24 @@ import AddTask from "./AddTask";
 //     </section>);
 // }
 
+// Creating some initial data in order to test
+// the updated Task component (we want to verify that
+// the task isComplete property can be successfully passed
+// down into the Task component as the initial checkbox state).
+const TODOS_DATA = [
+  { id: 1, text: 'Task 1', isComplete: true },
+  { id: 2, text: 'Task 2', isComplete: false },
+  { id: 3, text: 'Task 3', isComplete: false }
+];
+
 function TaskList(){
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(TODOS_DATA);
 
     const addTodo = todo => {
         if (!todo.text || /^\s*$/.test(todo.text)) {
           return;
         }
-        /**using the spread operator to pass my todo objects into an array */
+        /**using the spread operator to pass my todo objects into an array */    
         const newTodos = [todo, ...todos];
         /**setting my todos to an the new array above */
         setTodos(newTodos);
@@ -33,6 +43,11 @@ function TaskList(){
       const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
           if (todo.id === id) {
+            // In order for the useEffect hook inside of the Task
+            // component to detect when a task object has changed
+            // we need to create a copy of the object before
+            // we update it.
+            todo = {...todo};
             todo.isComplete = !todo.isComplete;
           }
           return todo;
@@ -41,7 +56,10 @@ function TaskList(){
       };
 
     return (<div><section className="task-container">
-        <Task todos={todos} completeTodo={completeTodo}/>
+      {/* The TaskList component should render the list of todos */}
+      {todos.map(t => (
+        <Task key={t.id} todo={t} completeTodo={completeTodo}/>
+      ))}
     </section>
     <AddTask onSubmit={addTodo}/>
     </div>);
